285be58ab14fa0cefdf451ac78f25a8f230f1e21
LoicBlanchard7 <90914694+LoicBlanchard7@users.noreply.github.com>
2023-04-11 11:34:45 +0200

    Changement graphique

--- flutter/geofencing/lib/pages/ChoixParcours.dart
+++ flutter/geofencing/lib/pages/ChoixParcours.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:geofencing/pages/map.dart';
@@ -15,49 +17,55 @@ class _MyAppState extends State<ChoixParcours> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/fond_choix.jpg'),
-            fit: BoxFit.cover,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/fond_choix.jpg'),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        padding: const EdgeInsets.only(left: 50.0, right: 50.0),
-        child: Column(
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: const FittedBox(
-                fit: BoxFit.cover,
-                child: Text(
-                  '\n\nBienvenue dans la visite\nextérieur de la mine\nde Neuves-Maison.\n',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                    fontStyle: FontStyle.normal,
-                    shadows: [Shadow(blurRadius: 3, color: Colors.black)],
+          child: ClipRRect(
+              child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+            child: Container(
+              color: Colors.grey.withOpacity(0.1),
+              padding: const EdgeInsets.only(left: 40.0, right: 40.0),
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: const FittedBox(
+                      fit: BoxFit.cover,
+                      child: Text(
+                        '\n\nBienvenue dans la visite\nextérieur de la mine\nde Neuves-Maison.\n',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontStyle: FontStyle.normal,
+                          shadows: [Shadow(blurRadius: 3, color: Colors.black)],
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                  const Tableau(),
+                  Container(
+                    margin: const EdgeInsets.only(
+                        left: 50.0, right: 50.0, top: 30.0, bottom: 50.0),
+                    child: const Text(
+                      "Le prochain point conseillé s'affichera en rouge.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Colors.white,
+                        shadows: [Shadow(blurRadius: 3, color: Colors.black)],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            const Tableau(),
-            Container(
-              margin: const EdgeInsets.only(
-                  left: 50.0, right: 50.0, top: 30.0, bottom: 50.0),
-              child: const Text(
-                "Le prochain point conseillé s'affichera en rouge",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
-                  color: Colors.white,
-                  shadows: [Shadow(blurRadius: 3, color: Colors.black)],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+          ))),
     );
   }
 }
@@ -81,12 +89,14 @@ class Tableau extends StatelessWidget {
       TableRow(
         children: [
           Container(
-            padding: const EdgeInsets.only(left: 10.0),
-            color: Colors.blue,
-            child: Text(
-              'Choisissez votre parcours',
-              style:
-                  DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
+            color: const Color.fromARGB(255, 72, 68, 68),
+            child: Center(
+              child: Text(
+                'Choisissez\nvotre parcours',
+                style: DefaultTextStyle.of(context)
+                    .style
+                    .apply(fontSizeFactor: 2.0, color: Colors.white),
+              ),
             ),
           )
         ],
